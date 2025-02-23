0647ab51dabc8217410feae34892cd7d9a8532b5
Albotica <alboticaconnect@gmail.com>
2025-02-23 20:12:46 +0530

    Update footer-section.tsx

--- src/components/ui/footer-section.tsx
+++ src/components/ui/footer-section.tsx
@@ -75,7 +75,7 @@ function Footerdemo() {
             <h3 className="mb-4 text-lg font-semibold">Contact Us</h3>
             <address className="space-y-2 text-sm not-italic">             
               {/* <p>Phone: (123) 456-7890</p> */}
-              <p>Email: alboticaconnect@gmail.com</p>
+              <p>Email: connect@albotica.live</p>
             </address>
           </div>
           <div className="relative">
